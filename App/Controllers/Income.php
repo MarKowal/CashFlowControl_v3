7abb8a50d62872721a453c183a6739294628aeeb
Marcin Kowalski <marcin.wojciech.kowalski@gmail.com>
2023-04-18 20:56:21 +0200

    Display the defaut income category on list

--- App/Controllers/Income.php
+++ App/Controllers/Income.php
@@ -9,46 +9,14 @@ use App\Models\Earning;
 class Income extends Authenticated{
 
     public function newAction(){
-       // $incomeCategories = [];
-
-        /*foreach(Earning::getDefaultIncomeCategories() as category){
-           $incomeCategories[] => category;
-        }*/
-       // $incomeCategories = Earning::getDefaultIncomeCategories();
-        //$incomeCategories = Earning::getDefaultIncomeCategories();
-        //var_dump($_SESSION);
-        //var_dump($_POST);
-
-        echo "<pre>";
-        //var_dump($incomeCategories);
-        //echo "<br><br><br>";
-       // foreach($incomeCategories as $value){
-       //     print_r($value); 
-       // }
-       // print_r(Earning::getDefaultIncomeCategories());
-
+      
         $incomeCategories = [];
         $incomeCategories = Earning::getDefaultIncomeCategories();
 
-        var_dump($incomeCategories);
-
-        echo '<br>'.$incomeCategories[1];
-
-        View::renderTemplate('Income/new.html');
+        View::renderTemplate('Income/new.html', [
+            'categories' => $incomeCategories
+        ]);
     }
-
-    //metoda ktora pobierze kategorie z DB jako tabelę i prześle je do html do wyświetlenia
-    //public czy protected?
-    /*public function getDefaultIncomeCategories(){
-        
-        //$earning = new Earning();
-        $incomeCategories = [];
-
-        $incomeCategories[] = Earning::$earning->getDefaultIncomeCategories();
-
-
-    }*/
-
 
     //zapisanie danych z formularza income.html do bazy danych
     public function createAction(){
--- App/Models/Earning.php
+++ App/Models/Earning.php
@@ -9,14 +9,10 @@ class Earning extends \Core\Model{
     public static function getDefaultIncomeCategories(){
 
         $sql = 'SELECT * FROM incomes_category_default';
-        //$sql = 'SELECT name_income FROM incomes_category_default';
         $db = static::getDB();
         $stmt = $db->prepare($sql);
         $stmt->execute();
         
-        //echo "<pre>"; 
-        //var_dump($stmt->fetchAll());
-
         return $stmt->fetchAll(PDO::FETCH_COLUMN, 1);
 
     }
