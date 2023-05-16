b90804cbc30ad9906d2c4c7a08ef6b313c260112
Marcin Kowalski <marcin.wojciech.kowalski@gmail.com>
2023-05-16 21:35:10 +0200

    Choose and validate the other dates

--- App/Controllers/BalanceSheet.php
+++ App/Controllers/BalanceSheet.php
@@ -5,7 +5,8 @@ namespace App\Controllers;
 use \Core\View;
 use App\Models\Earning;
 use App\Models\Expenditure;
-use DateTime; //używam wbudowanej klasy PHP DateTime()
+use DateTime; //używam wbudowanej w PHP klasy DateTime()
+use App\Flash;
 
 
 class BalanceSheet extends Authenticated{
@@ -18,20 +19,37 @@ class BalanceSheet extends Authenticated{
 
     public function newAction(){
         View::renderTemplate('BalanceSheet/new.html');
-        
+        $this->indicateTimePeriod();
+    }
+
+    protected function indicateTimePeriod(){
+
         $this->timePeriod = $_POST['timePeriod'] ?? NULL;
-       
+
+        $this->selectedStartDate = new DateTime(); 
+        $this->selectedEndDate = new DateTime();
+
+        if(isset($_POST['dateStart'])){
+            echo $this->selectedStartDateString = $_POST['dateStart'].'<br>';
+            echo $this->selectedEndDateString = $_POST['dateEnd'].'<br>';
+
+            if($this->validateStartAndEndDates() == true){
+                echo 'both the Other Dates are good';
+                exit();
+                //teraz mam dwie zwalidowane daty od usera - początkową i końcową
+            }
+        }
+
         if($this->timePeriod != NULL){
             $this->transferTimePeriodIntoDate();
-            echo  $this->selectedStartDateString.'<br>';
-            echo $this->selectedEndDateString;
         }
+
+        echo $this->selectedStartDateString.'<br>';
+        echo $this->selectedEndDateString;
     }
 
     protected function transferTimePeriodIntoDate(){
 
-        $this->selectedStartDate = new DateTime(); 
-        $this->selectedEndDate = new DateTime(); 
         $this->selectedEndDate->modify('now');
 
         if($this->timePeriod == 'presentMonth'){
@@ -46,12 +64,31 @@ class BalanceSheet extends Authenticated{
         } elseif($this->timePeriod == 'presentYear'){
 
             $this->selectedStartDate->modify('1 January this year');
+
+        } elseif($this->timePeriod == 'otherTime'){
+
+            $this->redirect('/BalanceSheet/getOtherTime');
+           
         }
 
         $this->selectedStartDateString = $this->selectedStartDate->format('Y-m-d');
         $this->selectedEndDateString = $this->selectedEndDate->format('Y-m-d');
     }
     
+    public function getOtherTime(){
+        View::renderTemplate('BalanceSheet/new.html', [
+            'choosenOtherTime' => 1
+        ]);
+    }
+
+    protected function validateStartAndEndDates(){
+        if($this->selectedStartDateString < $this->selectedEndDateString){
+            return true;
+        } else {
+            Flash::addMessages('Sorry, start-date cannot be later than end-date. Try again.', 'warning');
+            $this->redirect('/BalanceSheet/new');
+        }
+    }
 }
 
 ?>
